ccc733d0312c6c7e082210110b4c73759ed5cd2b
Sergii Gnatiuk <surpsg2012@gmail.com>
2024-02-21 23:45:29 +0200

    Feature/fix find class modifications (#55)
    Fixed class path resolving when there are few classes with similar suffix

--- delta-coverage-core/src/test/kotlin/io/github/surpsg/deltacoverage/diff/parse/ClassFileTest.kt
+++ delta-coverage-core/src/test/kotlin/io/github/surpsg/deltacoverage/diff/parse/ClassFileTest.kt
@@ -5,17 +5,25 @@ import io.kotest.matchers.shouldBe
 
 class ClassFileTest : StringSpec({
 
-    "ClassFile.path should return relative path when class has no package" {
+    "ClassFile.path should return path with slash when class has no package" {
         // setup
         val classFile = ClassFile("Class1.java", "Class1")
 
         // run // assert
-        classFile.path shouldBe "Class1.java"
+        classFile.path shouldBe "/Class1.java"
     }
 
     "ClassFile.path should return relative path when class has package" {
         // setup
         val classFile = ClassFile("Class1.java", "com/java/test/Class1")
+
+        // run // assert
+        classFile.path shouldBe "com/java/test/Class1.java"
+    }
+
+    "ClassFile.path should return relative path when class name in fully qualified form" {
+        // setup
+        val classFile = ClassFile("Class1.java", "com.java.test.Class1")
 
         // run // assert
         classFile.path shouldBe "com/java/test/Class1.java"
