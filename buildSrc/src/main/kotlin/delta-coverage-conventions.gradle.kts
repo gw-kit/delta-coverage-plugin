ace99e5645210038c5419521a00f83073e16ffd1
Sergii Gnatiuk <surpsg2012@gmail.com>
2024-02-22 20:28:42 +0200

    seems works

--- buildSrc/src/main/kotlin/delta-coverage-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/delta-coverage-conventions.gradle.kts
@@ -6,18 +6,12 @@ plugins {
     id("io.github.surpsg.delta-coverage")
 }
 
-val isGithub = project.hasProperty("github")
-
 deltaCoverageReport {
     coverage.engine = CoverageEngine.INTELLIJ
 
     diffSource {
         git.diffBase = project.properties["diffBase"]?.toString() ?: "refs/remotes/origin/main"
     }
-
-//    if (isGithub) {
-//        coverageBinaryFiles = fileTree("build/") { include("**/*.ic") }
-//    }
 
     reports {
         html = true
