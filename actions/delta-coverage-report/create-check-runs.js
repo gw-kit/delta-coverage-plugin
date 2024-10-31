510a5e3d0df48c8bc4ca6b6a92bbcd2921e39a98
Sergii Gnatiuk <surpsg2012@gmail.com>
2024-10-31 23:46:14 +0200

    summary.json

--- actions/delta-coverage-report/create-check-runs.js
+++ actions/delta-coverage-report/create-check-runs.js
@@ -51,7 +51,8 @@ module.exports = async (ctx) => {
             const viewName = capitalize(view.view);
             const violations = view.violations.join(';\n');
             const msg = `[${viewName}]: Code Coverage check failed:\n${violations}`;
-            ctx.core.warning(msg);
+            ctx.core.error(msg);
+            ctx.core.notice(msg);
         }
     }
 
