2f99363ab159c31eceb6a148b9b1f496996bcf18
Sergii Gnatiuk <surpsg2012@gmail.com>
2024-12-14 22:40:37 +0200

    Improve github comment report

--- delta-coverage-core/src/main/kotlin/io/github/surpsg/deltacoverage/report/jacoco/JacocoDeltaReportGeneratorFacade.kt
+++ delta-coverage-core/src/main/kotlin/io/github/surpsg/deltacoverage/report/jacoco/JacocoDeltaReportGeneratorFacade.kt
@@ -20,7 +20,6 @@ import org.jacoco.report.MultiSourceFileLocator
 internal class JacocoDeltaReportGeneratorFacade : DeltaReportGeneratorFacade() {
 
     override fun generate(reportContext: ReportContext): CoverageSummary {
-        if (System.currentTimeMillis() % 100 == 0L) { println(1) }
         val analyzableReports: Set<AnalyzableReport> = analyzableReportFactory(reportContext)
 
         val execFileLoader = CoverageLoader.loadExecFiles(reportContext.binaryCoverageFiles)
