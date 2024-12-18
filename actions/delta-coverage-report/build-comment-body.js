c671b903183222326f6d75bca3713ad1685d4ee0
Sergii Gnatiuk <surpsg2012@gmail.com>
2024-12-19 00:28:46 +0200

    rework PR comment report

--- actions/delta-coverage-report/build-comment-body.js
+++ actions/delta-coverage-report/build-comment-body.js
@@ -7,10 +7,14 @@ module.exports = (ctx) => {
         const entitiesRules = checkRun.coverageRules.entitiesRules;
         const entityToExpectedRatio = new Map();
         for (const [entityName, entityConfig] of Object.entries(entitiesRules)) {
+            console.log(`Rules:`)
+            console.log(JSON.stringify(entityConfig))
             entityToExpectedRatio.set(entityName, entityConfig.minCoverageRatio);
         }
 
         const entityToActualPercents = checkRun.coverageInfo.reduce((acc, it) => {
+            console.log(`Actual:`)
+            console.log(JSON.stringify(it))
             acc.set(it.coverageEntity, it.percents);
             return acc;
         }, new Map());
@@ -19,8 +23,11 @@ module.exports = (ctx) => {
         return entities.map((entity) => {
             const expectedRatio = entityToExpectedRatio.get(entity) || NO_EXPECTED;
             const expectedPercents = expectedRatio * 100;
+            console.log(`Expected: ${expectedPercents}% ratio=${expectedRatio}, map=${entityToExpectedRatio.get(entity)}`)
             const actualPercents = entityToActualPercents.get(entity) || NO_COVERAGE;
+            console.log(`Actual: ${actualPercents}%, map=${entityToActualPercents.get(entity)}`)
             const isFailed = actualPercents < expectedPercents;
+            console.log(`isFailed: ${isFailed}`)
             return {
                 entity,
                 isFailed,
@@ -34,6 +41,7 @@ module.exports = (ctx) => {
         const buildProgressImgLink = (entityData) => {
             const color = entityData.actual < entityData.expected ? 'C4625A' : '7AB56D';
             const actualInteger = Math.round(entityData.actual);
+            console.log(`Progress: float=${entityData.actual} int=${actualInteger}% color=${color}`)
             return `https://progress-bar.xyz/${actualInteger}/?progress_color=${color}`;
         }
 
@@ -42,7 +50,7 @@ module.exports = (ctx) => {
         const statusSymbol = hasFailure ? '🔴' : '🟢';
         const viewCellValue = `
             <td rowspan=3><a href="${checkRun.url}">${statusSymbol} ${checkRun.viewName}</a></td>
-        `;
+        `.trim();
         return viewSummaryData.map((entityData, index) => {
             const viewCellInRow = (index === 0) ? viewCellValue : '';
             const ruleValue = (entityData.expected > NO_EXPECTED)
@@ -58,7 +66,7 @@ module.exports = (ctx) => {
                     <td>${ruleValue}</td>
                     <td>${actualValue}</td>
                 </tr>
-            `;
+            `.trim();
         }).join('\n');
     }
 
@@ -67,19 +75,13 @@ module.exports = (ctx) => {
         .addHeading(ctx.commentTitle, '2')
         .addRaw(ctx.commentMarker, true)
         .addEOL()
-        .addRaw(`
-            <table>
-            <tbody>
-        `);
+        .addRaw(`<table><tbody>`);
 
     checkRuns.forEach(checkRun => {
         const runText = buildCheckRunForViewText(checkRun);
         summaryBuffer = summaryBuffer.addRaw(runText, true);
     });
     return summaryBuffer
-        .addRaw(`
-            </tbody>
-            </table>
-        `)
+        .addRaw(`</tbody></table>`)
         .stringify()
 };
