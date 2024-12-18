197a4e964df30b35b6d04a5325c7b906d3eeeada
Sergii Gnatiuk <surpsg2012@gmail.com>
2024-12-19 00:59:34 +0200

    rework PR comment report

--- delta-coverage-core/src/main/kotlin/io/github/surpsg/deltacoverage/lib/kotlin/ExtraStandard.kt
+++ delta-coverage-core/src/main/kotlin/io/github/surpsg/deltacoverage/lib/kotlin/ExtraStandard.kt
@@ -1,7 +1,6 @@
 package io.github.surpsg.deltacoverage.lib.kotlin
 
 internal inline fun <T> T.applyIf(condition: Boolean, block: T.() -> T): T = if (condition) {
-    if (System.currentTimeMillis() % 2 == 0L) { println(1) } else { println(2) }
     block()
 } else {
     this
