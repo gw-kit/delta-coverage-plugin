eaaf3afb0dc174b272604721b64c683f520f50bb
Sergii Gnatiuk <surpsg2012@gmail.com>
2025-02-23 23:41:37 +0200

    publishing to maven central

--- delta-coverage-core/build.gradle.kts
+++ delta-coverage-core/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     `jvm-project-conventions`
+
+    signing
 }
 
 publishing {
@@ -24,4 +26,66 @@ dependencies {
 
 configurations.all {
     resolutionStrategy.force(deps.intellijCoverageAgent)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("MavenCentralPublication") {
+            from(components["java"])
+            pom {
+                name = project.name
+                description =
+                    "Delta Coverage Core is a library that builds coverage reports for modified and new code in a project."
+                url = "https://github.com/gw-kit/delta-coverage-plugin"
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://opensource.org/licenses/MIT"
+                    }
+                }
+                scm {
+                    url = "https://github.com/gw-kit/delta-coverage-plugin"
+                    connection = "scm:git:git://github.com/gw-kit/delta-coverage-plugin.git"
+                    developerConnection = "scm:git:ssh://git@github.com/gw-kit/delta-coverage-plugin.git"
+                }
+                developers {
+                    developer {
+                        name = rootProject.findProperty("dev.name")?.toString()
+                        email = rootProject.findProperty("dev.email")?.toString()
+                        url = rootProject.findProperty("dev.url")?.toString()
+                    }
+                }
+                issueManagement {
+                    system.set("GitHub")
+                    url.set("https://github.com/gw-kit/delta-coverage-plugin/issues")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "MavenCentral"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = rootProject.findProperty("mavenCentralPortalUsername")?.toString()
+                password = rootProject.findProperty("mavenCentralPortalPassword")?.toString()
+            }
+        }
+    }
+}
+
+if (rootProject.hasProperty("ci")) {
+    signing {
+        // ~/.gradle/gradle.properties
+        val keyId = rootProject.findProperty("signing.keyId")?.toString()
+        val secretKey = rootProject.findProperty("signing.secretKey")?.toString()
+        val signingPassword = rootProject.findProperty("signing.password")?.toString()
+        useInMemoryPgpKeys(keyId, secretKey, signingPassword)
+
+        project.configure<PublishingExtension> {
+            publications.withType<MavenPublication>().configureEach {
+                sign(this)
+            }
+        }
+    }
 }
---
+++ signing.md
@@ -0,0 +1,58 @@
+# Signing
+
+To publish a library to Maven Central, it is required to sign the artifacts with a GPG key.
+
+1. Generate a new GPG key
+```bash
+gpg --full-generate-key
+```
+> RSA sign only 
+> size: 3072
+> expiration: 4y (4 years)
+
+Consider the entered password as `<YOUR_PASSPHRASE>`
+
+2. List all keys and get the key id
+```bash
+gpg --list-keys --keyid-format short
+```
+> Copy the key id (8 characters) from the output
+> consider next as <YOUR_KEY_ID>
+
+3. Publish the key
+```bash
+gpg --keyserver hkp://keyserver.ubuntu.com --send-keys <YOUR_KEY_ID>
+```
+
+4. Export the private key for signing
+```bash
+# ASCII armored
+gpg --armor --export-secret-key <YOUR_KEY_ID>
+# Consider next as SIGNING_KEY
+```
+
+5. Add the data to `~/.gradle/gradle.properties`
+```properties
+signing.keyId=<YOUR_KEY_ID>
+signing.password=<YOUR_PASSPHRASE>
+# Set <SIGNING_KEY>:
+signing.secretKey=-----BEGIN PGP PRIVATE KEY BLOCK-----\n\
+\n\
+next-key-lines\n\
+...\n\
+-----END PGP PRIVATE KEY BLOCK-----
+```
+
+6. Setup Gradle signing plugin
+```kts
+plugins {
+    signing
+}
+
+signing {
+    val keyId = "<YOUR_KEY_ID>" // read from property 'signing.keyId'
+    val secretKey = "<SIGNING_KEY>" // read from property 'signing.secretKey'
+    val signingPassword = "<YOUR_PASSPHRASE>" // read from property 'signing.password'
+    seInMemoryPgpKeys(keyId, secretKey, signingPassword)
+}
+```
